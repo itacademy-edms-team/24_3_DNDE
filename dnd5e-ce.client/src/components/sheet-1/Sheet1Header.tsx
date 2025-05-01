a09c589a55b1d35214bc24e2cbbc2b4271a7b41e
Aleksandr <alexlysakovich@gmail.com>
2025-05-01 20:43:23 +0700

    Add selectors to Sheet1Header

--- dnd5e-ce.client/src/components/sheet-1/Sheet1Header.tsx
+++ dnd5e-ce.client/src/components/sheet-1/Sheet1Header.tsx
@@ -1,27 +1,30 @@
-﻿import React from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { updateName, updateClass, updateLevel, updateRace, updateBackstory, updateWorldview, updatePlayerName, updateExperience } from '../../store/sheet1Slice';
-
-import Container from 'react-bootstrap/Container';
+﻿import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import { RootState } from '../../types/state';
+
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { updateName, updateClass, updateLevel, updateRace, updateBackstory, updateWorldview, updatePlayerName, updateExperience } from '../../store/sheet1Slice';
+import { selectCharacterBackstory, selectCharacterClass, selectCharacterExperience, selectCharacterLevel, selectCharacterName, selectCharacterPlayerName, selectCharacterRace, selectCharacterWorldview } from '../../store/selectors/sheet1Selectors';
 
 const Sheet1Header: React.FC = () => {
   const dispatch = useAppDispatch();
 
-  const chName = useAppSelector((state: RootState) => state.sheet1.name);
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const chName = useAppSelector(selectCharacterName);
+  const handleNameChange = (e: React.FocusEvent<HTMLInputElement>) => {
     dispatch(updateName(e.target.value));
   }
-
-  const chClass = useAppSelector((state: RootState) => state.sheet1.class);
+  const handleNameBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    dispatch(updateName(e.target.value));
+  }
+  
+  const chClass = useAppSelector(selectCharacterClass);
   const handleClassChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updateClass(e.target.value));
   }
 
-  const chLevel = useAppSelector((state: RootState) => state.sheet1.level);
+  const chLevel = useAppSelector(selectCharacterLevel);
   const handleLevelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = Number(e.target.value);
     if (!isNaN(newValue)) {
@@ -29,29 +32,29 @@ const Sheet1Header: React.FC = () => {
     }
   }
 
-  const chRace = useAppSelector((state: RootState) => state.sheet1.race);
+  const chRace = useAppSelector(selectCharacterRace);
   const handleRaceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updateRace(e.target.value));
   }
 
-  const chBackstory = useAppSelector((state: RootState) => state.sheet1.backstory);
+  const chBackstory = useAppSelector(selectCharacterBackstory);
   const handleBackstoryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updateBackstory(e.target.value));
   }
 
-  const chWorldView = useAppSelector((state: RootState) => state.sheet1.worldview);
+  const chWorldView = useAppSelector(selectCharacterWorldview);
   const handleWorldviewChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updateWorldview(e.target.value));
   }
 
-  const chPlayerName = useAppSelector((state: RootState) => state.sheet1.playerName);
+  const chPlayerName = useAppSelector(selectCharacterPlayerName);
   const handlePlayerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updatePlayerName(e.target.value));
   }
 
-  const chExperience = useAppSelector((state: RootState) => state.sheet1.experience);
+  const chExperience = useAppSelector(selectCharacterExperience);
   const handleExperienceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = Number(e.target.value);
+    const newValue = parseInt(e.target.value);
     if (!isNaN(newValue)) {
       dispatch(updateExperience(newValue));
     }
@@ -65,6 +68,7 @@ const Sheet1Header: React.FC = () => {
             <Form.Control
               value={chName}
               onChange={handleNameChange}
+              onBlur={handleNameBlur} // In futures will be used for sendind data to backend
             />
             <Form.Label className="text-uppercase fw-bold">Имя персонажа</Form.Label>
           </Form.Group>
