f6c29edad771e4d4ff3231f92f37fc1d5f06f019
Aleksandr <alexlysakovich@gmail.com>
2025-12-05 21:47:48 +0700

    Add DeleteIncomeTransactioService. Reson: keep example

---
+++ FinanceTrack.Finance/FinanceTrack.Finance/src/FinanceTrack.Finance.Core/Interfaces/IDeleteIncomeTransactionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTrack.Finance.Core.Interfaces;
+
+public interface IDeleteIncomeTransactionService
+{
+  Task<Result> DeleteIncomeTransaction(
+    Guid transactionId,
+    string userId,
+    CancellationToken cancellationToken = default
+  );
+}
--- FinanceTrack.Finance/FinanceTrack.Finance/src/FinanceTrack.Finance.UseCases/Transactions/Incomes/Delete/DeleteIncomeTransactionHandler.cs
+++ FinanceTrack.Finance/FinanceTrack.Finance/src/FinanceTrack.Finance.UseCases/Transactions/Incomes/Delete/DeleteIncomeTransactionHandler.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FinanceTrack.Finance.Core.Interfaces;
 using FinanceTrack.Finance.Core.TransactionAggregate;
 
 namespace FinanceTrack.Finance.UseCases.Transactions.Delete;
 
-public sealed class DeleteIncomeTransactionHandler(IRepository<Transaction> repository)
+public sealed class DeleteIncomeTransactionHandler(IDeleteIncomeTransactionService _service)
   : ICommandHandler<DeleteIncomeTransactionCommand, Result>
 {
   public async Task<Result> Handle(
@@ -15,19 +16,10 @@ public sealed class DeleteIncomeTransactionHandler(IRepository<Transaction> repo
     CancellationToken cancellationToken
   )
   {
-    var transaction = await repository.GetByIdAsync(request.TransactionId, cancellationToken);
-
-    if (transaction is null)
-      return Result.NotFound();
-
-    if (!string.Equals(transaction.UserId, request.UserId, StringComparison.Ordinal))
-      return Result.Forbidden();
-
-    if (transaction.TransactionType != TransactionType.Income)
-      return Result.Error("The specified transaction is not an income transaction.");
-
-    await repository.DeleteAsync(transaction, cancellationToken);
-
-    return Result.Success();
+    return await _service.DeleteIncomeTransaction(
+      request.TransactionId,
+      request.UserId,
+      cancellationToken
+    );
   }
 }
