e452eb63729570c2d43ce0295cf092a35d7c6af6
Aleksandr <alexlysakovich@gmail.com>
2025-12-15 00:52:39 +0700

    Fix migration errors

--- FinanceTrack.Finance/src/FinanceTrack.Finance.Infrastructure/Data/Migrations/20251214174906_InitialCreate.cs
+++ FinanceTrack.Finance/src/FinanceTrack.Finance.Infrastructure/Data/Migrations/20251214174906_InitialCreate.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
 namespace FinanceTrack.Finance.Infrastructure.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class RenameTransaction : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Transactions");
+            migrationBuilder.CreateTable(
+                name: "Contributors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber_CountryCode = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber_Number = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber_Extension = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contributors", x => x.Id);
+                });
 
             migrationBuilder.CreateTable(
                 name: "FinancialTransactions",
@@ -64,52 +79,10 @@ namespace FinanceTrack.Finance.Infrastructure.Data.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "Contributors");
+
+            migrationBuilder.DropTable(
                 name: "FinancialTransactions");
-
-            migrationBuilder.CreateTable(
-                name: "Transactions",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    IncomeTransactionId = table.Column<Guid>(type: "uuid", nullable: true),
-                    Amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
-                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW() AT TIME ZONE 'UTC'"),
-                    IsMonthly = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
-                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
-                    OperationDate = table.Column<DateOnly>(type: "date", nullable: false),
-                    TransactionType = table.Column<int>(type: "integer", nullable: false),
-                    UserId = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Transactions", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Transactions_Transactions_IncomeTransactionId",
-                        column: x => x.IncomeTransactionId,
-                        principalTable: "Transactions",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_IncomeTransactionId",
-                table: "Transactions",
-                column: "IncomeTransactionId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_OperationDate",
-                table: "Transactions",
-                column: "OperationDate");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_TransactionType",
-                table: "Transactions",
-                column: "TransactionType");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_UserId",
-                table: "Transactions",
-                column: "UserId");
         }
     }
 }
