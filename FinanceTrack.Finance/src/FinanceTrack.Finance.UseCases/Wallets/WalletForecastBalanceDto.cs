2d1fb64cb323154acb6c291a5a73d84dabcb5815
Aleksandr <alexlysakovich@gmail.com>
2026-03-24 17:30:58 +0700

    refactor: Make WalletForecastBalanceDto sealed. Remove unused usings

--- FinanceTrack.Finance/src/FinanceTrack.Finance.UseCases/Wallets/WalletForecastBalanceDto.cs
+++ FinanceTrack.Finance/src/FinanceTrack.Finance.UseCases/Wallets/WalletForecastBalanceDto.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FinanceTrack.Finance.UseCases.Wallets;
 
-namespace FinanceTrack.Finance.UseCases.Wallets;
-
-public record WalletForecastBalanceDto(decimal BalanceToday, decimal BalanceAtTheEndOfMonth);
+public sealed record WalletForecastBalanceDto(decimal BalanceToday, decimal BalanceAtTheEndOfMonth);
